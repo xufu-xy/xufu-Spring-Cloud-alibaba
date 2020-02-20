82da04cf2cd191e24fe635a8c2b870ef744cbcf7
lixiang1990 <swxg888@126.com>
2020-02-20 22:52:56 +0800

    commit by lixiang for first commit!

---
+++ spring-cloud-alibaba-gateway/src/main/java/com/xufu/service/DynamicRouteServiceImplByNacos.java
@@ -0,0 +1,58 @@
+package com.xufu.service;
+
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.xufu.entity.NacosGatewayProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.stereotype.Component;
+import com.xufu.utils.JsonUtils;
+
+import java.util.List;
+import java.util.concurrent.Executor;
+
+@Component
+public class DynamicRouteServiceImplByNacos implements CommandLineRunner {
+
+    @Autowired
+    private DynamicRouteServiceImpl dynamicRouteService;
+
+    @Autowired
+    private NacosGatewayProperties nacosGatewayProperties;
+
+    /**
+     * 监听Nacos Server下发的动态路由配置
+     */
+    public void dynamicRouteByNacosListener (){
+        try {
+            ConfigService configService= NacosFactory.createConfigService(nacosGatewayProperties.getAddress());
+            String content = configService.getConfig(nacosGatewayProperties.getDataId(), nacosGatewayProperties.getGroupId(), nacosGatewayProperties.getTimeout());
+            System.out.println(content);
+            configService.addListener(nacosGatewayProperties.getDataId(), nacosGatewayProperties.getGroupId(), new Listener()  {
+                @Override
+                public void receiveConfigInfo(String configInfo) {
+                    List<RouteDefinition> list = JsonUtils.toList(configInfo, RouteDefinition.class);
+                    list.forEach(definition->{
+                        dynamicRouteService.update(definition);
+                    });
+                }
+                @Override
+                public Executor getExecutor() {
+                    return null;
+                }
+            });
+        } catch (NacosException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        dynamicRouteByNacosListener();
+
+    }
+
+}
